6408e190149900cdf06fc59db543c69e46253ec0
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2025-03-10 12:03:52 +0100

    Commit v3

--- src/main/java/org/example/exameninterfaces/Controller/WebController.java
+++ src/main/java/org/example/exameninterfaces/Controller/WebController.java
@@ -2,7 +2,12 @@ package org.example.exameninterfaces.Controller;
 
 
 import org.example.exameninterfaces.repository.ItemRepository;
+import org.example.exameninterfaces.services.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +20,8 @@ public class WebController {
 
     @Autowired
     ItemRepository itemRepository;
+    @Autowired
+    private ReportService reportService;
 
 
     @GetMapping("/items/")
@@ -31,6 +38,16 @@ public class WebController {
         model.addAttribute("titulo", item.getTitle());
         model.addAttribute("item", item);
         return "indexItemById";
+    }
+
+    @GetMapping("/items/descargar")
+    public ResponseEntity<ByteArrayResource> descargarInforme(){
+        byte[] informe = reportService.generarInforme();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Informe.pdf");
+
+        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(new ByteArrayResource(informe));
     }
 
 }
---
+++ src/main/java/org/example/exameninterfaces/services/ReportService.java
@@ -0,0 +1,42 @@
+package org.example.exameninterfaces.services;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.example.exameninterfaces.model.Item;
+import org.example.exameninterfaces.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.List;
+
+@Service
+public class ReportService {
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ReportService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+
+    public byte[] generarInforme() {
+        try {
+            List<Item> listaItems = itemRepository.findAll();
+
+            InputStream report = getClass().getResourceAsStream("/reports/store_data.csv");
+
+            JasperReport reportJasper = JasperCompileManager.compileReport(report);
+
+            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(listaItems);
+
+            JasperPrint jp = JasperFillManager.fillReport(reportJasper, null,dataSource);
+
+            return JasperExportManager.exportReportToPdf(jp);
+
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
